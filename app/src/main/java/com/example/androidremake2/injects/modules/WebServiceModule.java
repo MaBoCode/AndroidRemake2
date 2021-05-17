df98025686c42819a366c9b9bde4e05365a8d949
Matthias Brown <matthias.brown@epita.fr>
2021-05-17 23:48:47 +0200

    refactor: add default headers for podcast service requests

--- app/src/main/java/com/example/androidremake2/injects/modules/WebServiceModule.java
+++ app/src/main/java/com/example/androidremake2/injects/modules/WebServiceModule.java
@@ -1,15 +1,26 @@
 package com.example.androidremake2.injects.modules;
 
+import android.content.Context;
+
+import com.example.androidremake2.R;
 import com.example.androidremake2.core.podcast.PodcastService;
 import com.example.androidremake2.core.user.UserService;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,8 +31,26 @@ public class WebServiceModule {
 
     @Provides
     @Singleton
-    public OkHttpClient provideOkHttpClient() {
-        return new OkHttpClient();
+    public OkHttpClient provideOkHttpClient(@ApplicationContext Context context) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(new Interceptor() {
+            @NotNull
+            @Override
+            public Response intercept(@NotNull Chain chain) throws IOException {
+                Request original = chain.request();
+
+                String appName = context.getString(R.string.app_name);
+                String listenAPIKey = context.getString(R.string.listen_api_key);
+
+                Request request = original.newBuilder()
+                        .header("User-Agent", appName)
+                        .header("X-ListenAPI-Key", listenAPIKey)
+                        .method(original.method(), original.body())
+                        .build();
+                return chain.proceed(request);
+            }
+        });
+        return builder.build();
     }
 
     @Provides
