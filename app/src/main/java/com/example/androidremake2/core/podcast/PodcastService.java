4e33763d8fa40ec9db7dfcf968053144fb672554
Matthias Brown <matthias.brown@epita.fr>
2021-05-17 23:46:45 +0200

    refactor: PodcastService

--- app/src/main/java/com/example/androidremake2/core/podcast/PodcastService.java
+++ app/src/main/java/com/example/androidremake2/core/podcast/PodcastService.java
@@ -2,29 +2,29 @@ package com.example.androidremake2.core.podcast;
 
 import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface PodcastService {
 
-    @Headers("X-ListenAPI-Key: 79387f0c059f4290aa717f23b2f1356c")
-    @GET("best_podcasts?page=1")
-    Observable<PodcastList> getBestPodcasts();
+    @GET("best_podcasts")
+    Observable<GetBestPodcastsResponse> getBestPodcasts(
+            @Query("page") Integer page,
+            @Query("region") String region
+    );
 
-    @Headers("X-ListenAPI-Key: 79387f0c059f4290aa717f23b2f1356c")
     @GET("podcasts/{podcastId}")
     Observable<Podcast> getPodcast(
             @Path("podcastId") String podcastId
     );
 
-    @Headers("X-ListenAPI-Key: 79387f0c059f4290aa717f23b2f1356c")
     @GET("search")
     Observable<SearchPodcastList> searchPodcasts(
             @Query("q") String query,
             @Query("type") String type,
             @Query("only_in") String onlyIn,
-            @Query("offset") Integer offset
+            @Query("offset") Integer offset,
+            @Query("region") String region
     );
 
 }
